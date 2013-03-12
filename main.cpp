86cea7667fa820ae2951126d065ffa73cfe0af25
Lasath Fernando <kde@lasath.org>
2013-03-12 15:09:31 +1100

    Initial Commit

--- main.cpp
+++ main.cpp
@@ -1,4 +1,39 @@
 #include <iostream>
+#include <chrono>
+#include <boost/date_time.hpp>
+#include <time.h>
+
+class Record {
+    std::string instrument;
+    std::chrono::time_point timestamp;
+    Type type;
+    double price;
+    std::size_t volume;
+    std::size_t undisclosedVolume;
+    std::size_t value() {
+        return volume * price;
+    }
+    std::string qualifiers;
+    std::string transId;
+
+    std::size_t bidId;
+    std::size_t askId;
+
+    BidAsk bidAsk;
+    
+
+    enum BidAsk {
+        Bid,
+        Ask
+    };
+
+    enum class Type {
+        ENTER,
+        AMEND,
+        DELETE,
+        TRADE
+    };
+};
 
 int main(int argc, char **argv) {
     std::cout << "Hello, world!" << std::endl;
