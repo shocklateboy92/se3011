5143bb017fdea004d3263cccd65aa76ab642067e
Christopher Manouvrier <cmanou@mac.com>
2013-04-09 12:43:36 +1000

    Trading Engine Complete v1

--- qt5/tradingengine.cpp
+++ qt5/tradingengine.cpp
@@ -1,5 +1,18 @@
 #include "tradingengine.h"
+#include "record.h"
+#include <iostream>
 
 TradingEngine::TradingEngine()
 {
+
+}
+
+
+void evaluateRecord(Record r) {
+    if (r.type == Record::TRADE){
+        std::cout << r << endl;
+    } else if (r.type == Record::ENTER && (r.askId == 0 || r.bidId == 0)) {
+        r.type = Record::TRADE;
+        std::cout << r << endl;
+    }
 }
