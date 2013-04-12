bded45a52e74810bf9bf15504d2b0820cc534270
Lasath Fernando <kde@lasath.org>
2013-04-12 22:13:42 +1000

    Generate getters and setters for Record's attributes
    And also rename the (private) attributes to m_atributes.

--- qt5/record.cpp
+++ qt5/record.cpp
@@ -4,3 +4,103 @@ Record::Record()
 {
 }
 
+
+QString Record::instrument() const
+{
+    return m_instrument;
+}
+
+void Record::setInstrument(const QString &value)
+{
+    m_instrument = value;
+}
+
+QDate Record::date() const
+{
+    return m_date;
+}
+
+void Record::setDate(const QDate &value)
+{
+    m_date = value;
+}
+
+QTime Record::time() const
+{
+    return m_time;
+}
+
+void Record::setTime(const QTime &value)
+{
+    m_time = value;
+}
+
+Record::Type Record::type() const
+{
+    return m_type;
+}
+
+void Record::setType(const Type &value)
+{
+    m_type = value;
+}
+
+double Record::volume() const
+{
+    return m_volume;
+}
+
+void Record::setVolume(double value)
+{
+    m_volume = value;
+}
+
+double Record::value() const
+{
+    return m_value;
+}
+
+void Record::setValue(double value)
+{
+    m_value = value;
+}
+
+long Record::transactionId() const
+{
+    return m_transId;
+}
+
+void Record::setTransId(long value)
+{
+    m_transId = value;
+}
+
+long Record::bidId() const
+{
+    return m_bidId;
+}
+
+void Record::setBidId(long value)
+{
+    m_bidId = value;
+}
+
+long Record::askId() const
+{
+    return m_askId;
+}
+
+void Record::setAskId(long value)
+{
+    m_askId = value;
+}
+
+Record::BidAsk Record::bidOrAsk() const
+{
+    return m_bidOrAsk;
+}
+
+void Record::setBidOrAsk(const BidAsk &value)
+{
+    m_bidOrAsk = value;
+}
--- qt5/record.h
+++ qt5/record.h
@@ -2,6 +2,7 @@
 #define RECORD_H
 
 #include <QDataStream>
+#include <QDateTime>
 
 class Record
 {
@@ -21,17 +22,47 @@ public:
     Record();
     QDataStream& operator>>(QDataStream &in);
 
+    QString instrument() const;
+    void setInstrument(const QString &value);
+
+    QDate date() const;
+    void setDate(const QDate &value);
+
+    QTime time() const;
+    void setTime(const QTime &value);
+
+    Type type() const;
+    void setType(const Type &value);
+
+    double volume() const;
+    void setVolume(double value);
+
+    double value() const;
+    void setValue(double value);
+
+    long transactionId() const;
+    void setTransId(long value);
+
+    long bidId() const;
+    void setBidId(long value);
+
+    long askId() const;
+    void setAskId(long value);
+
+    BidAsk bidOrAsk() const;
+    void setBidOrAsk(const BidAsk &value);
+
 private:
-    QString instrument;
-    QDate date;
-    QTime time;
-    Type type;
-    double volume;
-    double value;
-    long transId;
-    long bidId;
-    long askId;
-    BidAsk bidOrAsk;
+    QString m_instrument;
+    QDate m_date;
+    QTime m_time;
+    Type m_type;
+    double m_volume;
+    double m_value;
+    long m_transId;
+    long m_bidId;
+    long m_askId;
+    BidAsk m_bidOrAsk;
 };
 
 #endif // RECORD_H
