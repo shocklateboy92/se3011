df3b698b49d7ad2d43dc291ded0a7828cf4ac05c
Lasath Fernando <kde@lasath.org>
2013-05-11 19:14:11 +1000

    Create case distinction
    This is currently 'mockup code' - it doesn't compile, doesn't
    belong where it is, but shows (roughly) the logic.

--- trading-engine.cpp
+++ trading-engine.cpp
@@ -9,6 +9,53 @@ TradingEngine::TradingEngine(QObject *parent) :
 }
 
 void TradingEngine::processNewRecord(const Record &r) {
+    switch (r.type()) {
+
+    case Record::Type::ENTER:
+        switch (r.bidOrAsk()) {
+        case Record::BidAsk::Bid:
+            enterBid(Bid(r));
+            break;
+        case Record::BidAsk::Ask:
+            enterAsk(Ask(r));
+            break;
+        default:
+            qWarning() << "encountered record with Type ENTER, but is neither a Bid nor Ask";
+        }
+        break;
+    case Record::Type::DELETE:
+        switch (r.bidOrAsk()) {
+        case Record::BidAsk::Ask:
+            removeAsk(r.askId());
+            break;
+        case Record::BidAsk::Bid:
+            removeBid(r.bidId());
+            break;
+        default:
+            qWarning() << "trying to delete a record that is neither a Bid nor Ask";
+        }
+        break;
+    case Record::Type::AMEND:
+        switch (r.bidOrAsk()) {
+        case Record::BidAsk::Bid:
+            amendBid(r.bidId(), r.price(), r.volume());
+            break;
+        case Record::BidAsk::Ask:
+            amendAsk(r.askId(), r.price(), r.volume());
+            break;
+        default:
+            qWarning() << "trying to amend a record that is neither a Bid nor Ask";
+            break;
+        }
+        break;
+
+    case Record::Type::TRADE:
+        createTrade(Trade(r));
+        break;
+
+    default:
+        break;
+    }
     if (r.type() == Record::Type::TRADE) {
         emit newTradeCreated(Trade(r));
     } else if (r.type() == Record::Type::ENTER && (r.askId() == 6666 || r.bidId() == 6666)) {
