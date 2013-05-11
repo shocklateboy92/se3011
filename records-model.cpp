03dcd85c2bd84e40532adb4abdf5ac05cefeb90c
Lasath Fernando <kde@lasath.org>
2013-05-12 03:47:56 +1000

    Replace switch-case with hashmap of getters
    It (and all its supporting code) currently resides in
    records-model.cpp. It should be moved somewhere more useful.

--- records-model.cpp
+++ records-model.cpp
@@ -1,7 +1,11 @@
 #include "records-model.h"
 
+#include <QVariant>
 #include <QDebug>
 #include <QList>
+
+#include <functional>
+#include <unordered_map>
 
 enum class ColumnName {
     Instrument,
@@ -24,6 +28,37 @@ enum class ColumnName {
     SellerBrokerID
 };
 
+struct cHash {
+    std::size_t operator()(ColumnName n) const {
+        return std::hash<int>()(static_cast<int>(n));
+    }
+};
+
+template <typename Return, typename Object>
+QVariant mVar5(std::function<Return(const Object&)> f, Object *o) {
+    return QVariant(f(*o));
+}
+
+template <typename T>
+std::function<QVariant(Record *r)>
+O_(std::function<T(const Record&)> f) {
+    return std::bind(&mVar5<T, Record>, f, std::placeholders::_1);
+}
+
+static const std::unordered_map<ColumnName,
+std::function<QVariant(Record*)>, cHash> column_getters  {
+    {ColumnName::Instrument, O_<QString>(&Record::instrument)},
+    {ColumnName::Date, O_<QDate>(&Record::date)},
+    {ColumnName::Time, O_<QTime>(&Record::time)},
+
+    {ColumnName::Price, O_<double>(&Record::price)},
+    {ColumnName::Volume, O_<double>(&Record::volume)},
+    {ColumnName::Value, O_<double>(&Record::value)},
+    {ColumnName::TransID, O_<qint64>(&Record::transactionId)},
+    {ColumnName::BidID, O_<qint64>(&Record::bidId)},
+    {ColumnName::AskID, O_<qint64>(&Record::askId)}
+};
+
 static const QList<QString> column_strings = {
     "Instrument",
     "Date",
@@ -44,6 +79,8 @@ static const QList<QString> column_strings = {
     "Buyer Broker ID",
     "Seller Broker ID"
 };
+
+
 
 RecordsModel::RecordsModel(QObject *parent) :
     QAbstractTableModel(parent)
@@ -93,49 +130,28 @@ QVariant RecordsModel::data(const QModelIndex &index, int role) const
     if (role == Qt::DisplayRole && index.isValid()) {
         Record r = m_data.at(index.row());
 
-        switch (static_cast<ColumnName>(index.column())) {
-        case ColumnName::Instrument:
-            data = r.instrument();
-            break;
-        case ColumnName::Date:
-            data = r.date();
-            break;
-        case ColumnName::Time:
-            data = r.time();
-            break;
-        case ColumnName::RecordType:
-            if (r.type() == Record::Type::AMEND) {
-                data = "Amend";
-            } else if (r.type() == Record::Type::ENTER) {
-                data = "Enter";
-            } else if (r.type() == Record::Type::DELETE) {
-                data = "Delete";
-            } else if (r.type() == Record::Type::TRADE) {
-                data = "Trade";
-            } else {
-                data = "UNKNOWN";
+        ColumnName col = static_cast<ColumnName>(index.column());
+        if (column_getters.count(col) > 0) {
+            data = column_getters.at(col)(&r);
+        } else {
+            switch (static_cast<ColumnName>(index.column())) {
+
+            case ColumnName::RecordType:
+                if (r.type() == Record::Type::AMEND) {
+                    data = "Amend";
+                } else if (r.type() == Record::Type::ENTER) {
+                    data = "Enter";
+                } else if (r.type() == Record::Type::DELETE) {
+                    data = "Delete";
+                } else if (r.type() == Record::Type::TRADE) {
+                    data = "Trade";
+                } else {
+                    data = "UNKNOWN";
+                }
+                break;
+            default:
+                break;
             }
-            break;
-        case ColumnName::Price:
-            data = r.price();
-            break;
-        case ColumnName::Volume:
-            data = r.volume();
-            break;
-        case ColumnName::Value:
-            data = r.value();
-            break;
-        case ColumnName::TransID:
-            data = qlonglong(r.transactionId());
-            break;
-        case ColumnName::BidID:
-            data = qlonglong(r.bidId());
-            break;
-        case ColumnName::AskID:
-            data = qlonglong(r.askId());
-            break;
-        default:
-            break;
         }
     }
 
