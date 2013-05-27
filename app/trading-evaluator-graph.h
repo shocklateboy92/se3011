625a9aa29887b46de1fdd3174d7e3acad2040f7c
Christopher Manouvrier <cmanou@mac.com>
2013-05-27 15:16:06 +1000

    Trying to fix graph

--- app/trading-evaluator-graph.h
+++ app/trading-evaluator-graph.h
@@ -2,7 +2,11 @@
 #define TRADINGEVALUATORGRAPH_H
 
 #include <QDockWidget>
+#include <QInputDialog>
+#include <QLineEdit>
+#include <QMenu>
 #include <order.h>
+#include "qcustomplot.h"
 
 
 namespace Ui {
@@ -20,6 +24,18 @@ public:
 public slots:
     void plotNew(const Trade &trade);
 
+private slots:
+  void titleDoubleClick();
+  void axisLabelDoubleClick(QCPAxis* axis, QCPAxis::SelectablePart part);
+  void legendDoubleClick(QCPLegend* legend, QCPAbstractLegendItem* item);
+  void selectionChanged();
+  void mousePress();
+  void mouseWheel();
+  void removeSelectedGraph();
+  void removeAllGraphs();
+  void contextMenuRequest(QPoint pos);
+  void moveLegend();
+  void graphClicked(QCPAbstractPlottable *plottable);
 
 private:
     Ui::TradingEvaluatorGraph *ui;
