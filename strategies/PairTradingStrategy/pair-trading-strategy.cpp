246c6d0c481ef09b3d74125e0bc1df0571833e34
Vincent Wong <ck.vincentwong@gmail.com>
2013-05-28 00:39:26 +1000

    fix my as

--- strategies/PairTradingStrategy/pair-trading-strategy.cpp
+++ strategies/PairTradingStrategy/pair-trading-strategy.cpp
@@ -1,10 +1,40 @@
 #include "pair-trading-strategy.h"
+#include "pair-trading-widget.h"
 
+QDockWidget *PairTradingStrategy::configWidget()
+{
+    auto widget = new PairTradingWidget();
+    connect(widget, &PairTradingWidget::newPair,
+            this, &PairTradingStrategy::processPair);
+    connect(widget, &PairTradingWidget::deletePair,
+            this, &PairTradingStrategy::removePair);
 
-PairTradingStrategy::PairTradingStrategy()
+    return widget;
+}
+
+void PairTradingStrategy::processTrade(const Trade &trade)
+{
+}
+
+PairTradingStrategy::PairTradingStrategy(QObject *parent)
+    : QObject (parent)
 {
     
     
     
     
+}
+
+
+void PairTradingStrategy::processPair(const PairTradingStrategy::PairData pd)
+{
+}
+
+void PairTradingStrategy::removePair(const PairTradingStrategy::PairData pd)
+{
+}
+
+
+void PairTradingStrategy::reset()
+{
 }
