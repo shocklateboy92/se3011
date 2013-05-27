c21fe295d4b9a62606b157882079483f01d9eeb4
Vincent Wong <ck.vincentwong@gmail.com>
2013-05-27 18:27:57 +1000

    pair trade header

--- strategies/PairTradingStrategy/pair-trading-strategy.h
+++ strategies/PairTradingStrategy/pair-trading-strategy.h
@@ -2,12 +2,50 @@
 #define PAIRTRADINGSTRATEGY_H
 
 #include "pairtradingstrategy_global.h"
+#include <trading-strategy.h>
+#include <QtPlugin>
 
 class PAIRTRADINGSTRATEGYSHARED_EXPORT PairTradingStrategy
+        : public QObject, TradingStrategy
 {
-    
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID PAIR_TRADE_STRATEGY_TYPE_ID)
+    Q_INTERFACES(TradingStrategy)
+
 public:
-    PairTradingStrategy();
+    PairTradingStrategy(QObject *parent = 0);
+
+    QDockWidget* configWidget();
+
+    void processTrade(const Trade &trade);
+
+ signals:
+    void newRecordCreate(Record::Ptr r);
+
+private slots:
+    void processPair(const QString &instrument,
+                         const QString &volume,
+                         const QString &change);
+    void removePair(const QString &instrument);
+
+private:
+    class PairData;
+    QList<PairData> m_pairs;
+
+
+};
+
+class PairTradingStrategy::PairData {
+public:
+        double historicalSpread;
+        double previousPrice1;
+        double previousPrice2;
+        bool bought;
+        bool sold;
+
+
+
+
 };
 
 #endif // PAIRTRADINGSTRATEGY_H
